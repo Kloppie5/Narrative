0bb34ecdd1a45f16ba27f905f1c049565d6d3467
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-01-13 16:09:56 +0100

    Some data collection

--- Monster Hunter World/MonsterHunterWorldSaveFileEditor.cs
+++ Monster Hunter World/MonsterHunterWorldSaveFileEditor.cs
@@ -80,47 +80,110 @@ namespace MonsterHunterWorld {
             UInt64 end = start + 0x209AC0;
             Console.WriteLine($"Character {character} [{start:X} - {end:X}]");
 
-            hex_dump(data, start, end, 32, (UInt64 i) => {
-                UInt64 pos = i - start;
-                if (pos >= 0x000004 && pos < 0x000044) return ConsoleColor.Green; // Name
-                if (pos >= 0x000044 && pos < 0x000048) return ConsoleColor.Green; // HR
-                if (pos >= 0x000048 && pos < 0x00004C) return ConsoleColor.Green; // MR
-                if (pos >= 0x00004C && pos < 0x000050) return ConsoleColor.Green; // Zenny
-                if (pos >= 0x000050 && pos < 0x000054) return ConsoleColor.Green; // Research Points
-                if (pos >= 0x000054 && pos < 0x000058) return ConsoleColor.Green; // HR XP
-                if (pos >= 0x000058 && pos < 0x00005C) return ConsoleColor.Green; // MR XP
-                if (pos >= 0x00005C && pos < 0x000060) return ConsoleColor.Green; // Playtime
-                if (pos >= 0x000064 && pos < 0x0000DC) return ConsoleColor.Yellow; // Hunter Appearance
-                // 0x0000DC - 0x0002B2
-                if (pos >= 0x0002B2 && pos < 0x00211D) return ConsoleColor.Yellow; // Guildcard
-                if (pos >= 0x00211D && pos < 0x0C02E9) return ConsoleColor.Yellow; // Guildcards | 100 slots
-                if (pos >= 0x0F3510 && pos < 0x10A710) return ConsoleColor.Yellow; // Item Loadouts | 80 slots
-                if (pos >= 0x116098 && pos < 0x116158) return ConsoleColor.Yellow; // Item Pouch | 24 slots
-                if (pos >= 0x116158 && pos < 0x1161D8) return ConsoleColor.Yellow; // Ammo Pouch | 16 slots
-                if (pos >= 0x1161D8 && pos < 0x116298) return ConsoleColor.Yellow; // Material Pouch | 24 slots
-                if (pos >= 0x116298 && pos < 0x1162F8) return ConsoleColor.Yellow; // Special Pouch | 12 slots
-                if (pos >= 0x1162F8 && pos < 0x116938) return ConsoleColor.Yellow; // Item Box | 200 slots
-                if (pos >= 0x116938 && pos < 0x116F78) return ConsoleColor.Yellow; // Ammo Box | 200 slots
-                if (pos >= 0x116F78 && pos < 0x119688) return ConsoleColor.Yellow; // Material Box | 1250 slots
-                if (pos >= 0x119688 && pos < 0x11A628) return ConsoleColor.Yellow; // Decoration Box | 500 slots
-                if (pos >= 0x11A628 && pos < 0x1674A0) return ConsoleColor.Yellow; // Equipment Box | 2500 slots
-                if (pos >= 0x1674A0 && pos < 0x176FA4) return ConsoleColor.DarkYellow; // -1 Equipment | 510 slots (probably 500 and 10 of different types)
-                if (pos >= 0x176FA4 && pos < 0x19D6E0) return ConsoleColor.DarkYellow; // Empty Equipment | 1250 slots (maybe used after filling the main equipment box?)
+            Console.WriteLine($"  Signature [{start:X} - {start + 0x4:X}]");
+            Console.WriteLine($"  Name [{start + 0x4:X} - {start + 0x44:X}]");
+            Console.WriteLine($"  HR [{start + 0x44:X} - {start + 0x48:X}]");
+            Console.WriteLine($"  MR [{start + 0x48:X} - {start + 0x4C:X}]");
+            Console.WriteLine($"  Zenny [{start + 0x4C:X} - {start + 0x50:X}]");
+            Console.WriteLine($"  Research Points [{start + 0x50:X} - {start + 0x54:X}]");
+            Console.WriteLine($"  HR XP [{start + 0x54:X} - {start + 0x58:X}]");
+            Console.WriteLine($"  MR XP [{start + 0x58:X} - {start + 0x5C:X}]");
+            Console.WriteLine($"  Playtime [{start + 0x5C:X} - {start + 0x60:X}]");
+            Console.WriteLine($"  Hunter Appearance [{start + 0x60:X} - {start + 0xDC:X}]");
 
-                if (pos >= 0x1A8D48 && pos < 0x1ACD48) return ConsoleColor.Yellow; // NPC Conversations | 2048 slots
+            hex_dump(data, start + 0x0000DC, start + 0x0002B2, 32, (_) => {return ConsoleColor.Yellow;});
+            // Palico appearance
+            Console.ReadLine();
 
-                if (pos >= 0x1AD5DF && pos < 0x1B177F) return ConsoleColor.Yellow; // Investigations | 400 slots
-                if (pos >= 0x1B21D1 && pos < 0x1B60D1) return ConsoleColor.DarkYellow; // Something? | 128 slots
+            Console.WriteLine($"  Guildcard [{start + 0x0002B2:X} - {start + 0x00211D:X}] <0x1E6B>");
+            Console.WriteLine($"  Guildcards [{start + 0x00211D:X} - {start + 0x0C02E9:X}] | 100 slots <0x1E6B>");
 
-                if (pos >= 0x1B6955 && pos < 0x1DB8D5) return ConsoleColor.Yellow; // Equipment Layouts | 224 slots
-                if (pos >= 0x1DB8D5 && pos < 0x1E4315) return ConsoleColor.DarkYellow; // Something? | 112 slots
-                if (pos >= 0x1E4315 && pos < 0x1E5ED5) return ConsoleColor.DarkYellow; // Something? | 24 slots
-                if (pos >= 0x1E7082 && pos < 0x1EA202) return ConsoleColor.DarkYellow; // Something? | 96 slots
+            hex_dump(data, start + 0x0C02E9, start + 0x0F3510, 32, (_) => {return ConsoleColor.Yellow;});
+            // 20 slots of something, same length as guildcards, so maybe a buffer for received guildcards?
+            // multiple giant tables
+            // << small monster kills
+            // many slots of something else
+            // padding
+            // large float holding structure
+            Console.ReadLine();
 
-                if (pos >= 0x2098C0 && pos < 0x209AC0) return ConsoleColor.Green; // Hash
+            Console.WriteLine($"  Item Loadouts [{start + 0x0F3510:X} - {start + 0x10A710:X}] | 80 slots");
 
-                return ConsoleColor.Red;
-            });
+            hex_dump(data, start + 0x10A710, start + 0x116098, 32, (_) => {return ConsoleColor.Yellow;});
+            // completely empty
+            // followed by a bit increment till 77 followed by FF, possibly something like progress tracking <done on complete save>
+            Console.ReadLine();
+
+            Console.WriteLine($"  Item Pouch [{start + 0x116098:X} - {start + 0x116158:X}] | 24 slots");
+            Console.WriteLine($"  Ammo Pouch [{start + 0x116158:X} - {start + 0x1161D8:X}] | 16 slots");
+            Console.WriteLine($"  Material Pouch [{start + 0x1161D8:X} - {start + 0x116298:X}] | 24 slots");
+            Console.WriteLine($"  Special Pouch [{start + 0x116298:X} - {start + 0x1162F8:X}] | 12 slots");
+            Console.WriteLine($"  Item Box [{start + 0x1162F8:X} - {start + 0x116938:X}] | 200 slots");
+            Console.WriteLine($"  Ammo Box [{start + 0x116938:X} - {start + 0x116F78:X}] | 200 slots");
+            Console.WriteLine($"  Material Box [{start + 0x116F78:X} - {start + 0x119688:X}] | 1250 slots");
+            Console.WriteLine($"  Decoration Box [{start + 0x119688:X} - {start + 0x11A628:X}] | 500 slots");
+            Console.WriteLine($"  Equipment Box [{start + 0x11A628:X} - {start + 0x1674A0:X}] | 2500 slots");
+            Console.WriteLine($"  -1 Equipment [{start + 0x1674A0:X} - {start + 0x176FA4:X}] | 510 slots");
+            Console.WriteLine($"  Empty Equipment [{start + 0x176FA4:X} - {start + 0x19D6E0:X}] | 1250 slots");
+
+            hex_dump(data, start + 0x19D6E0, start + 0x1A8D48, 32, (_) => {return ConsoleColor.Yellow;});
+            // Weapon notice flags
+            Console.ReadLine();
+
+            Console.WriteLine($"  NPC Conversations [{start + 0x1A8D48:X} - {start + 0x1ACD48:X}] | 2048 slots");
+
+            hex_dump(data, start + 0x1ACD48, start + 0x1AD5DF, 32, (_) => {return ConsoleColor.Yellow;});
+            // Palico name is in here, so probably palico tool levels and xp
+            // Quest notics flags
+
+            // 1AD228:1 Notice flag; The Great Glutton
+			// 1AD227:40 Notice flag; Bird-Brained Bandit
+			// 1AD227:10 Notice flag; A Thicket of Thugs
+			// 1AD227:8 Notice flag; Butting Heads with Nature
+			// 1AD227:4 Notice flag; The Great Jagras Hunt
+			// 1AD227:2 Notice flag; A Kestadon Kerfuffle
+			// 1AD227:1 Notice flag; Jagras of the Great Forest
+
+			// 1AD249:4 Notice flag; Learning the Clutch
+
+            Console.ReadLine();
+
+            Console.WriteLine($"  Investigations [{start + 0x1AD5DF:X} - {start + 0x1B177F:X}] | 400 slots");
+
+            hex_dump(data, start + 0x1B177F, start + 0x1B21D1, 32, (_) => {return ConsoleColor.Yellow;});
+            // Map notice flags
+            Console.ReadLine();
+
+            Console.WriteLine($"  UNKNOWN [{start + 0x1B21D1:X} - {start + 0x1B60D1:X}] | 128 slots");
+            hex_dump(data, start + 0x1B60D1, start + 0x1B6955, 32, (_) => {return ConsoleColor.Yellow;});
+            Console.ReadLine();
+
+            Console.WriteLine($"  Equipment Layouts [{start + 0x1B6955:X} - {start + 0x1DB8D5:X}] | 224 slot");
+            Console.WriteLine($"  UNKNOWN [{start + 0x1DB8D5:X} - {start + 0x1E4315:X}] | 112 slot");
+            Console.WriteLine($"  Room Configurations [{start + 0x1E4315:X} - {start + 0x1E5ED5:X}] | 24 slot");
+
+            hex_dump(data, start + 0x1E5ED5, start + 0x1E7082, 32, (_) => {return ConsoleColor.Yellow;});
+            // Strange names, some from guildcards, some unknown
+            // looks like 12 slots of maybe guildcard linked palico helpers you can find?
+            // Guildcard titles and medals notics flags
+            Console.ReadLine();
+
+            Console.WriteLine($"  UNKNOWN [{start + 0x1E7082:X} - {start + 0x1EA202:X}] | 96 slot");
+
+            hex_dump(data, start + 0x1EA202, start + 0x2098C0, 32, (_) => {return ConsoleColor.Yellow;});
+            // very empty with the occasional structure
+            Console.ReadLine();
+
+            Console.WriteLine($"  Hash [{start + 0x2098C0:X} - {start + 0x209AC0:X}]");
+        }
+
+        public void save_character ( int character, String filename ) {
+            Int32 start = 0x3010D8 + character * 0x209AC0;
+
+            Byte[] data = new Byte[0x209AC0];
+            Array.Copy(this.data, start, data, 0, 0x209AC0);
+
+            File.WriteAllBytes(filename, data);
         }
 
         public void hex_dump ( Byte[] bytes, UInt64 start = 0, UInt64 end = 0, UInt64 line_length = 32, Func<UInt64, ConsoleColor> color_func = null ) {
@@ -137,9 +200,10 @@ namespace MonsterHunterWorld {
                     Console.Write(
                         i + j < end ?
                         $"{bytes[i + j]:X2} ".Replace("0", "-") :
-                        "    "
+                        "   "
                     );
                 }
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("  ");
                 for ( UInt64 j = 0; j < line_length && i + j < end; j++ ) {
                     Console.ForegroundColor = color_func(i + j);
@@ -149,6 +213,7 @@ namespace MonsterHunterWorld {
                         "."
                     );
                 }
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine();
             }
             Console.ForegroundColor = ConsoleColor.White;
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 using MonsterHunterWorld;
 
@@ -11,7 +12,10 @@ namespace Narrative {
             MonsterHunterWorldSaveFileEditor mhwsfe = new MonsterHunterWorldSaveFileEditor(@"C:\Program Files (x86)\Steam\userdata\49682378\582010\remote\SAVEDATA1000");
             mhwsfe.DecryptGlobal();
             mhwsfe.DecryptCharacters();
-            mhwsfe.dump_character(0);
+            // mhwsfe.dump_character(2);
+            String path = Directory.GetCurrentDirectory();
+            path = Path.Combine(path, "saves", $"character_p25_mjurder.sav");
+            mhwsfe.save_character(2, path);
         }
     }
 }
